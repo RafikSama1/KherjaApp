6e1fb01bcf20baa3c1a82958e9cf70587c71431c
RafikSama <rafik23r@gmail.com>
2024-06-25 13:29:38 +0200

    3ed commit

--- routes/voyages.routes.js
+++ routes/voyages.routes.js
@@ -4,5 +4,10 @@ const router = Router();
 const voyagesControllers = require("../controllers/voyages.contollers");
 
 router.get('/', voyagesControllers.index);
+router.get('/:id', voyagesControllers.show);
+router.post('/', voyagesControllers.store);
+router.put('/:id', voyagesControllers.update);
+router.delete('/:id', voyagesControllers.delete);
+
 
 module.exports = router;
